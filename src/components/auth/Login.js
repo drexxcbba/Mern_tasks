27c8ff074263afb6c6702087a7870de09522b663
unknown <villarroel24kyle@gmail.com>
2021-05-27 11:41:58 -0400

    ft: added form to login

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,8 +1,44 @@
 import React from 'react';
 
 const Login = () => {
+
+    const onChange = () => {
+
+    }
+
     return ( 
-        <h1>Login</h1>
+        <div className="form-usuario">
+            <div className="contenedor-form sombra-dark">
+                <h1>Iniciar Sesiòn</h1>
+                <form>
+                    <div className="campo-form">
+                        <label htmlFor="email">Email</label>
+                        <input 
+                            type="email"
+                            id="email"
+                            placeholder="Tu email"
+                            onChange={onChange}
+                        />
+                    </div>
+                    <div className="campo-form">
+                        <label htmlFor="password">Password</label>
+                        <input 
+                            type="password"
+                            id="password"
+                            placeholder="Tu password"
+                            onChange={onChange}
+                        />
+                    </div>
+                    <div className="campo-form">
+                        <input
+                            type="submit"
+                            className="btn btn-primario btn-block"
+                            value="Iniciar Sesiòn"
+                        />
+                    </div>
+                </form>
+            </div>
+        </div>
      );
 }
  
